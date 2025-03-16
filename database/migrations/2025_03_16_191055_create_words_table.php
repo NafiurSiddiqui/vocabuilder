c133e3856eb9caba1d36547addfbc41f2569720b
NafiurSiddiqui <saejim91@gmail.com>
2025-03-17 01:16:35 +0600

    feat(word processor): updates the db schema

--- database/migrations/2025_03_16_191055_create_words_table.php
+++ database/migrations/2025_03_16_191055_create_words_table.php
@@ -14,6 +14,13 @@ return new class extends Migration
         Schema::create('words', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('word')->unique();
+            $table->string('phonetic');
+            $table->json('pronunciation');
+            $table->json('definition');
+            $table->json('examples');
+            $table->string('category')->default('uncategorized');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
         });
     }
 
