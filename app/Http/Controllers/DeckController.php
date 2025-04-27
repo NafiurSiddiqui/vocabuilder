db412836fa8d5655ac2a7cd17242caba220b001c
NafiurSiddiqui <NafiurSiddiqui@users.noreply.github.com>
2025-04-28 02:59:31 +0600

    feat (deck) : Maintains data security

--- app/Http/Controllers/DeckController.php
+++ app/Http/Controllers/DeckController.php
@@ -10,7 +10,55 @@ class DeckController extends Controller
 {
     public function index()
     {
-        $decks = Deck::all();
+        $userId = auth()->id();
+        $decks = Deck::where('user_id', $userId)->get();
         return Inertia::render('inventory/index', ['decks' => $decks]);
+    }
+
+    public function create()
+    {
+        return Inertia::render('inventory/create');
+    }
+
+    public function store(Request $request)
+    {
+        $attributes = $request->validate([
+            'name' => ['required', 'min:3', 'max:255', 'unique:decks'],
+            'description' => ['nullable'],
+        ]);
+        $attributes['user_id'] = auth()->user()->id;
+
+        Deck::create($attributes);
+
+        return redirect()->route('inventory.index');
+    }
+
+    public function show(Deck $deck)
+    {
+        return Inertia::render('inventory/show', ['deck' => $deck]);
+    }
+
+    public function edit(Deck $deck)
+    {
+        // return Inertia::render('inventory/edit', ['deck' => $deck]);
+    }
+
+    public function update(Request $request, Deck $deck)
+    {
+        // $request->validate([
+        //     'name' => 'required',
+        //     'description' => 'required',
+        // ]);
+
+        // $deck->update($request->all());
+
+        // return redirect()->route('inventory.index');
+    }
+
+    public function destroy(Deck $deck)
+    {
+        // $deck->delete();
+
+        // return redirect()->route('inventory.index');
     }
 }
