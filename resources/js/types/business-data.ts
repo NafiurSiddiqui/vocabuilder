7b568cbbb48e2ff8a6410ad52848607eb733d316
NafiurSiddiqui <NafiurSiddiqui@users.noreply.github.com>
2025-04-27 19:36:06 +0600

    chore: creates types for business logic

---
+++ resources/js/types/business-data.ts
@@ -0,0 +1,21 @@
+export interface InventoryProps {
+    words: Array<{
+        id: number;
+        created_at: string;
+        updated_at: string;
+        word: string;
+        phonetic: string;
+        pronunciation: string;
+        definition: string;
+        examples: string;
+        category: string;
+        user_id: number;
+    }>;
+}
+
+export interface Definitions {
+    antonyms: Array<string>;
+    definition: string;
+    example: string;
+    synonyms: Array<string>;
+}
