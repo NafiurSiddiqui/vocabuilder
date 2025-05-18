02f6c3ae528322580cefe509ab2ba7a479ec3513
NafiurSiddiqui <NafiurSiddiqui@users.noreply.github.com>
2025-05-18 16:34:11 +0600

    feat: Adds Deck service for encapsulating business logic for Deck

---
+++ app/Services/DeckService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Services;
+
+
+use App\Models\Deck;
+use App\Enum\DeckEnum;
+use Illuminate\Support\Facades\Cache;
+
+class DeckService
+{
+    /**
+     * Retrieve and cache the default deck instance.
+     *
+     * The result is cached indefinitely. Use `Cache::forget('deck.default')`
+     * if the default deck is updated and needs to be refreshed.
+     * 
+     * 
+     * @return Deck
+     */
+    public function getDefaultDeck(): Deck
+    {
+
+        return Cache::rememberForever('deck.default', function () {
+            return Deck::where('name', DeckEnum::DEFAULT->value)->firstOrFail();
+        });
+    }
+}
