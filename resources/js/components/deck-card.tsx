e83799d87d38ad508411531b2f01e5001e803999
NafiurSiddiqui <NafiurSiddiqui@users.noreply.github.com>
2025-05-24 22:50:32 +0600

    chore: clean ups and udpates

--- resources/js/components/deck-card.tsx
+++ resources/js/components/deck-card.tsx
@@ -1,8 +1,8 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
-import { Button } from "./ui/button";
-import { Checkbox } from "./ui/checkbox";
-import { Plus } from "lucide-react";
-import { Link } from "@inertiajs/react";
+import { Link } from '@inertiajs/react';
+import { Plus } from 'lucide-react';
+import { Button } from './ui/button';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './ui/card';
+import { Checkbox } from './ui/checkbox';
 
 interface DeckCardProps {
     title: string;
@@ -13,23 +13,11 @@ interface DeckCardProps {
     onAdd?: () => void;
 }
 
-export function DeckCard({
-    title,
-    wordCount,
-    showCheckbox = false,
-    checked = false,
-    onCheck,
-    onAdd,
-}: DeckCardProps) {
+export function DeckCard({ title, wordCount, showCheckbox = false, checked = false, onCheck, onAdd }: DeckCardProps) {
     return (
         <Card>
             <CardHeader className="flex-row items-center justify-between gap-0">
-                <Button
-                    variant="outline"
-                    size="sm"
-                    className="flex items-center gap-2"
-                    onClick={onAdd}
-                >
+                <Button variant="outline" size="sm" className="flex items-center gap-2" onClick={onAdd}>
                     <Plus className="size-4" />
                     Add a word
                 </Button>
@@ -37,17 +25,16 @@ export function DeckCard({
                 <Checkbox />
             </CardHeader>
 
-
             <CardContent>
                 <Link href={route('inventory.show', title)}>
                     <CardTitle className="text-2xl">{title}</CardTitle>
                     {/* <span className="text-muted-foreground text-base font-medium"> */}
                     <CardDescription>
-                        {wordCount} word{wordCount !== 1 ? "s" : ""}
+                        {wordCount} word{wordCount !== 1 ? 's' : ''}
                     </CardDescription>
                 </Link>
                 {/* </span> */}
             </CardContent>
-        </Card >
+        </Card>
     );
 }
