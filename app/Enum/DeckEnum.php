e308a538b3ac1c0dd7dcab0a19e769f8ae8e53ed
NafiurSiddiqui <NafiurSiddiqui@users.noreply.github.com>
2025-05-24 22:41:32 +0600

    feat: adds new title and slug

--- app/Enum/DeckEnum.php
+++ app/Enum/DeckEnum.php
@@ -4,7 +4,7 @@ namespace App\Enum;
 
 enum DeckEnum: string
 {
-    case DEFAULT = 'tossed';
+    case DEFAULT_TITLE = 'Tossed';
+    case DEFAULT_SLUG = 'tossed';
     case DEFAULT_DESCRIPTION = "Default deck, where all the words are tossed into.";
-
 }
