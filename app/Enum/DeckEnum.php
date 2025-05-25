8f1abf42800f10d403d4b1ba5f0b88017e6459d3
NafiurSiddiqui <NafiurSiddiqui@users.noreply.github.com>
2025-05-25 21:04:35 +0600

    chore: updates default description

--- app/Enum/DeckEnum.php
+++ app/Enum/DeckEnum.php
@@ -6,5 +6,5 @@ enum DeckEnum: string
 {
     case DEFAULT_TITLE = 'Tossed';
     case DEFAULT_SLUG = 'tossed';
-    case DEFAULT_DESCRIPTION = "Default deck, where all the words are tossed into.";
+    case DEFAULT_DESCRIPTION = "Default deck - where all the uncateogrized words are tossed into.";
 }
