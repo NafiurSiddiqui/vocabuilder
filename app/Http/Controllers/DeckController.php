3babc6b78abe47645008d96276b40d26c7552244
NafiurSiddiqui <NafiurSiddiqui@users.noreply.github.com>
2025-05-26 00:02:14 +0600

    refactor: Implements user separated data logic

--- app/Http/Controllers/DeckController.php
+++ app/Http/Controllers/DeckController.php
@@ -5,37 +5,58 @@ namespace App\Http\Controllers;
 use App\Models\Deck;
 use Inertia\Inertia;
 use Inertia\Response;
+use App\Models\DefaultDeck;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Services\DeckService;
 use Illuminate\Support\Facades\Auth;
 
 
+//Make sure you are fetching only the data that belongs to the user
+
 class DeckController extends Controller
 {
+
+
     public function index()
     {
         $userId = Auth::user()->id;
-        $decks = Deck::forAuthedUser()
+
+
+        $decks = Deck::where('user_id', $userId)
             ->withCount('words') //this gives you the words_count
             ->orderBy('title', 'desc')
             ->simplePaginate(20);
 
-        // dd($decks->words_count);
-        return Inertia::render('inventory/index', ['deckItems' => $decks->items(), 'decks' => $decks]);
+
+        $defaultDeck = DefaultDeck::withCount([
+            'words as words_count' => function ($query) use ($userId) {
+                $query->where('user_id', $userId);
+            }
+        ])->firstOrFail();
+
+
+        return Inertia::render('inventory/index', ['deckItems' => $decks->items(), 'decks' => $decks, 'defaultDeck' => $defaultDeck]);
     }
 
     public function create()
     {
-        return Inertia::render('inventory/create');
+        //using a modal here
+
     }
 
     public function store(Request $request)
     {
+
         $attributes = $request->validate([
             'title' => ['required', 'min:3', 'max:255', 'unique:decks'],
             'description' => ['nullable'],
         ]);
+
+
         $attributes['user_id'] = Auth::user()->id;
         $attributes['title'] = ucfirst($attributes['title']);
+        $attributes['slug'] = Str::slug($attributes['title']);
         Deck::create($attributes);
 
         return redirect()->route('inventory.index');
@@ -43,9 +64,9 @@ class DeckController extends Controller
 
     public function show(Deck $deck): Response
     {
-        // dd($deck->words()->get());
+
         $words = $deck->words()->orderBy('title', 'desc')->simplePaginate(20);
-        // dd($words->items());
+
         return Inertia::render('inventory/show', ['deck' => $deck, 'words' => $words]);
     }
 
