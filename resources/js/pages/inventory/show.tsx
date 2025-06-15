9048b14f44d66a97382eb5d5fcb8e7f474b52cc6
NafiurSiddiqui <NafiurSiddiqui@users.noreply.github.com>
2025-06-16 01:58:36 +0600

    feat: render deck accordion cards by group (indexed)

--- resources/js/pages/inventory/show.tsx
+++ resources/js/pages/inventory/show.tsx
@@ -23,7 +23,7 @@ interface Props {
 }
 
 export default function Show({ deck, words }: Props) {
-    const dataparsed = JSON.parse(words['data'][0]['definition']);
+    // const dataparsed = JSON.parse(words['data'][0]['definition']);
     // console.log('data', dataparsed);
     // Try to extract title from the deck object or fallback to string
     const title = typeof deck === 'string' ? deck : (deck?.title ?? '');
@@ -38,14 +38,23 @@ export default function Show({ deck, words }: Props) {
             href: `/inventory/${encodeURIComponent(title)}`,
         },
     ];
+
+    const wordsTransformed = Object.entries(words);
+    //now you are getting object
+    console.log(typeof words);
+    console.log(words);
+    console.log(wordsTransformed);
     //TODO: props drilling starts with this deckTitle. Which the category dropdown needs on the card-details. useContext or some other ways to share data. NO PROPS DRILLING!
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title={`Inventory: ${title}`} />
             <section className="p-4">
-                {/* <h2 className="mb-4 text-2xl">{title}</h2> */}
                 <div className="w-full">
-                    <WordIndexAccordion Letter="A" wordCount={words['data'].length} words={words['data']} deckTitle={deck.title} />
+                    {/* <WordIndexAccordion Letter="A" wordCount={wordsTransformed.length} words={words['data']} deckTitle={deck.title} /> */}
+
+                    {wordsTransformed.map(([letter, words]) => (
+                        <WordIndexAccordion key={letter} Letter={letter} wordCount={words.length} words={words} deckTitle={deck.title} />
+                    ))}
                 </div>
             </section>
         </AppLayout>
