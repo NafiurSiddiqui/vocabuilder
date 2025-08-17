a5e36f8f771ba591c648607e76f3b3c463c8a255
NafiurSiddiqui <NafiurSiddiqui@users.noreply.github.com>
2025-08-17 20:47:58 +0600

    feat: add deletion of a word, provide type support

--- resources/js/components/cards/word-card-detail.tsx
+++ resources/js/components/cards/word-card-detail.tsx
@@ -1,11 +1,12 @@
 import { useDeckContext } from '@/context/DeckContext';
 import { capitalizeFirstLetter } from '@/lib/utils';
 import { Word } from '@/types/business-data';
-import { router, useForm } from '@inertiajs/react';
-import { Volume2 } from 'lucide-react';
+import { router, useForm, usePage } from '@inertiajs/react';
+import { CircleEllipsis, Volume2 } from 'lucide-react';
 import { useRef } from 'react';
 import InputError from '../input-error';
 import { Badge } from '../ui/badge';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '../ui/dropdown-menu';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../ui/select';
 import { Separator } from '../ui/separator';
 
@@ -14,22 +15,37 @@ interface Pronunciation {
     text: string;
 }
 
-interface Definition {
+interface WordDefinition {
     antonyms: string[];
-    definition: string;
-    example: string;
     synonyms: string[];
+    definition: string;
+    example?: string; // optional, since some defs might have examples
 }
 
-export default function WordCardDetail({ word, deckTitle }: { word: Word; deckTitle: string }) {
-    const wordParsed = JSON.parse(word['definition']);
+interface WordParseEntry {
+    antonyms: string[];
+    synonyms: string[];
+    definitions: WordDefinition[];
+    partOfSpeech: string;
+}
+
+type WordParse = WordParseEntry[];
+
+export default function WordCardDetail({ word }: { word: Word; deckTitle: string }) {
+    const wordParsed: WordParse = JSON.parse(word['definition']);
     const synonyms = wordParsed[0]['synonyms'];
     const pronunciation = JSON.parse(word.pronunciation);
     const { decks, defaultDeck } = useDeckContext();
 
     const deckItems = [...decks, defaultDeck];
 
-    const { data, setData, processing, errors, reset, recentlySuccessful, patch } = useForm({
+    const {
+        data,
+        processing,
+        errors,
+        wasSuccessful,
+        delete: destroy,
+    } = useForm({
         //word has default deck id or deck_id? set default_deck_id otherwise, deck_id
         deck_data:
             deckItems.length > 0
@@ -39,6 +55,8 @@ export default function WordCardDetail({ word, deckTitle }: { word: Word; deckTi
                 : '',
         word_id: word.id,
     });
+
+    const { errors: formErrors } = usePage().props;
 
     const deckUpdateHandler: (value: string) => void = (value) => {
         const selectedDeck = JSON.parse(value) as { id: number; slug: string };
@@ -52,120 +70,165 @@ export default function WordCardDetail({ word, deckTitle }: { word: Word; deckTi
         });
     };
 
+    const deleteHandler = () => {
+        // console.log('triggers');
+        console.log(word.id);
+        if (!word) {
+            console.warn('Provide word id for deletion');
+            return;
+        }
+        if (confirm('Are you sure you want to delete this word?')) {
+            destroy(route('word-processor.destroy', { word: word.id }), {
+                onError: (errors) => {},
+                onFinish: () => {
+                    console.log('Delete request finished (success or fail)');
+                },
+            });
+        }
+    };
+
     const audioRef = useRef<HTMLAudioElement>(null);
     const playAudio = () => {
         audioRef.current?.play();
     };
 
-    return wordParsed.map((wordParsed, index) => {
+    return wordParsed.map((wordParsed: WordParseEntry, index: number) => {
         return (
-            <article role="dialog" aria-modal="true" aria-labelledby="word-title" className="my-4 rounded-sm bg-stone-50 p-4" key={index + 1}>
-                {/* Header */}
-                <header className="flex items-start justify-between">
-                    {/* Left Side */}
-                    <div>
-                        <span className="text-sm text-gray-500">{wordParsed.partOfSpeech}</span>
-                        <h2 id="word-title" className="text-3xl font-bold">
-                            {capitalizeFirstLetter(word.title)}
-                        </h2>
-                        <div className="flex items-center gap-4">
-                            {pronunciation.map((pronunciation: Pronunciation, index: number) => {
-                                return (
-                                    <div key={index + 1} className="flex items-center">
-                                        <span>{pronunciation['audio'].includes('uk') ? '🇬🇧' : '🇺🇸'}</span>
-                                        <button
-                                            onClick={playAudio}
-                                            className="rounded-full p-2 hover:bg-gray-200 dark:hover:bg-gray-700"
-                                            aria-label="Play pronunciation"
-                                        >
-                                            <Volume2 className="h-5 w-5" />
-                                        </button>
+            <div key={index + 1}>
+                {formErrors.word_id && <p className="semibold text-red-500">Opps! {formErrors.word_id}</p>}
+                {processing && <p>Deleting...</p>}
+                {wasSuccessful && <p>Word updated successfully</p>}
+                {!processing && !formErrors.word_id && (
+                    <article role="dialog" aria-modal="true" aria-labelledby="word-title" className="my-4 rounded-sm bg-stone-50 p-4" key={index + 1}>
+                        {/* Header */}
+                        <header className="">
+                            {/* Dropdowns */}
+                            <div className="flex flex-wrap items-center justify-end gap-2">
+                                <div>
+                                    <Select name="deck_data" value={data.deck_data} onValueChange={deckUpdateHandler}>
+                                        <SelectTrigger className="cursor-pointer">
+                                            <SelectValue placeholder="Select a deck" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            {deckItems.map((deck) => (
+                                                <SelectItem
+                                                    key={deck.id}
+                                                    value={JSON.stringify({ id: deck.id, slug: deck.slug })}
+                                                    className="cursor-pointer"
+                                                >
+                                                    {deck.title}
+                                                </SelectItem>
+                                            ))}
+                                        </SelectContent>
+                                    </Select>
+                                    <InputError message={errors.deck_data} className="mt-2" />
+                                </div>
+                                <div>
+                                    {/* edit dropdown */}
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <CircleEllipsis className="h-5 w-5 cursor-pointer opacity-70 hover:opacity-100" />
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent>
+                                            <DropdownMenuItem
+                                                variant="destructive"
+                                                className="cursor-pointer"
+                                                onSelect={(e) => {
+                                                    e.preventDefault(); // prevent Radix from swallowing event
+                                                    deleteHandler();
+                                                }}
+                                            >
+                                                Delete Word
+                                            </DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </div>
+                            </div>
 
-                                        <span className="text-sm text-gray-700 dark:text-gray-300">{pronunciation['text']}</span>
+                            <div>
+                                <span className="text-sm text-gray-500">{wordParsed.partOfSpeech}</span>
+                                <h2 id="word-title" className="text-3xl font-bold">
+                                    {capitalizeFirstLetter(word.title)}
+                                </h2>
+                                <div className="flex items-center gap-4">
+                                    {pronunciation.map((pronunciation: Pronunciation, index: number) => {
+                                        return (
+                                            <div key={index + 1} className="flex items-center">
+                                                <span className="mr-1">{pronunciation['audio'].includes('uk') ? '🇬🇧' : '🇺🇸'}</span>
+                                                {pronunciation.audio && (
+                                                    <button
+                                                        onClick={playAudio}
+                                                        className="rounded-full p-2 hover:bg-gray-200 dark:hover:bg-gray-700"
+                                                        aria-label="Play pronunciation"
+                                                    >
+                                                        <Volume2 className="h-5 w-5" />
+                                                    </button>
+                                                )}
+                                                <span className="text-sm text-gray-700 dark:text-gray-300">{pronunciation['text']}</span>
+                                                {pronunciation['audio'] && <audio ref={audioRef} src={pronunciation['audio']} preload="none" />}
+                                            </div>
+                                        );
+                                    })}
+                                </div>
+                            </div>
+                        </header>
 
-                                        <audio ref={audioRef} src={pronunciation['audio']} preload="none" />
-                                    </div>
-                                );
-                            })}
-                        </div>
-                    </div>
-
-                    {/* Dropdown */}
-                    <div className="flex flex-col items-end gap-2">
-                        <div>
-                            {/* <Select name="deck_data" value={data.deck_data} onValueChange={(value) => setData('deck_data', value)}> */}
-
-                            <Select name="deck_data" value={data.deck_data} onValueChange={deckUpdateHandler}>
-                                <SelectTrigger>
-                                    <SelectValue placeholder="Select a deck" />
-                                </SelectTrigger>
-                                <SelectContent>
-                                    {deckItems.map((deck) => (
-                                        <SelectItem key={deck.id} value={JSON.stringify({ id: deck.id, slug: deck.slug })}>
-                                            {deck.slug}
-                                            {deck.id}
-                                            {deck.title}
-                                        </SelectItem>
-                                    ))}
-                                </SelectContent>
-                            </Select>
-                            <InputError message={errors.deck_data} className="mt-2" />
-                        </div>
-                    </div>
-                </header>
-
-                {/* Definitions */}
-                <section className="mt-6">
-                    <h3 className="text-xl font-semibold">Definitions</h3>
-                    <Separator />
-                    <ol className="mt-2 list-inside list-decimal space-y-4">
-                        {wordParsed.definitions &&
-                            wordParsed.definitions.map((definition: Definition, index: number) => {
-                                return (
-                                    <li key={index}>
-                                        {definition.definition}
-                                        {definition.example && (
-                                            <div className="mt-2 block border-l-4 p-2 text-sm text-gray-500 italic">"{definition.example}"</div>
-                                        )}
-                                    </li>
-                                );
-                            })}
-                    </ol>
-                </section>
-                {/* Synonyms */}
-                {wordParsed.synonyms.length > 0 && (
-                    <section className="mt-6">
-                        <h3 className="text-xl font-semibold">Synonyms</h3>
-                        <Separator />
-                        <ul className="mt-2 flex flex-wrap gap-2">
-                            {synonyms &&
-                                synonyms.map((synonym: string, index: number) => {
-                                    return (
-                                        <li key={index}>
-                                            <Badge>{synonym}</Badge>
-                                        </li>
-                                    );
-                                })}
-                        </ul>
-                    </section>
+                        {/* Definitions */}
+                        <section className="mt-6">
+                            <h3 className="text-xl font-semibold">Definitions</h3>
+                            <Separator />
+                            <ol className="mt-2 list-inside list-decimal space-y-4">
+                                {wordParsed.definitions &&
+                                    wordParsed.definitions.map((definition, index) => {
+                                        return (
+                                            <li key={index}>
+                                                {definition.definition}
+                                                {definition.example && (
+                                                    <div className="mt-2 block border-l-4 p-2 text-sm text-gray-500 italic">
+                                                        "{definition.example}"
+                                                    </div>
+                                                )}
+                                            </li>
+                                        );
+                                    })}
+                            </ol>
+                        </section>
+                        {/* Synonyms */}
+                        {wordParsed.synonyms.length > 0 && (
+                            <section className="mt-6">
+                                <h3 className="text-xl font-semibold">Synonyms</h3>
+                                <Separator />
+                                <ul className="mt-2 flex flex-wrap gap-2">
+                                    {synonyms &&
+                                        synonyms.map((synonym: string, index: number) => {
+                                            return (
+                                                <li key={index}>
+                                                    <Badge>{synonym}</Badge>
+                                                </li>
+                                            );
+                                        })}
+                                </ul>
+                            </section>
+                        )}
+                        {/* Antonyms */}
+                        {wordParsed.antonyms.length > 0 && (
+                            <section className="mt-6">
+                                <h3 className="text-xl font-semibold">Antonyms</h3>
+                                <Separator />
+                                <ul className="mt-2 flex flex-wrap gap-2">
+                                    {wordParsed.antonyms.map((antonym: string, index: number) => {
+                                        return (
+                                            <li key={index}>
+                                                <Badge>{antonym}</Badge>
+                                            </li>
+                                        );
+                                    })}
+                                </ul>
+                            </section>
+                        )}
+                    </article>
                 )}
-                {/* Antonyms */}
-                {wordParsed.antonyms.length > 0 && (
-                    <section className="mt-6">
-                        <h3 className="text-xl font-semibold">Antonyms</h3>
-                        <Separator />
-                        <ul className="mt-2 flex flex-wrap gap-2">
-                            {wordParsed.antonyms.map((antonym: string, index: number) => {
-                                return (
-                                    <li key={index}>
-                                        <Badge>{antonym}</Badge>
-                                    </li>
-                                );
-                            })}
-                        </ul>
-                    </section>
-                )}
-            </article>
+            </div>
         );
     });
 }
